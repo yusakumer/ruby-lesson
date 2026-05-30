87131ed72eda3066dfb222585dbff197d3963611
yusaku <s.yusaku1220@gmail.com>
2026-05-30 23:38:21 +0900

    ruby構文練習

--- resson-ruby/resson.rb
+++ resson-ruby/resson.rb
@@ -8,4 +8,14 @@ format_names = names.map do |name|
   "#{name[:family_name]} #{name[:first_name]}"
 end
 
-puts names.any?{|e| e[:first_name] == "taro"}
+puts names.any? { |e| e[:first_name] == "taro" }
+
+s = gets.chomp.chars
+hash = Hash.new(0)
+
+s.each do |n|
+  hash[n] += 1
+end
+
+key, value = hash.max_by { |k, v| v }
+puts "#{key} #{value}"
