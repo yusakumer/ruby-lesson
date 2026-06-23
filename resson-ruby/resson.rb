eecf1266a8071562c2faaa5c4820a6412c65639c
yusaku <s.yusaku1220@gmail.com>
2026-06-23 22:12:10 +0900

    ruby 練習

--- resson-ruby/resson.rb
+++ resson-ruby/resson.rb
@@ -1,21 +1,9 @@
-names = [
-  { first_name: 'taro', family_name: 'sato' },
-  { first_name: 'rika', family_name: 'yamada' }
-]
-# 出力値 sato taro, yamada rika
+require 'csv'
 
-format_names = names.map do |name|
-  "#{name[:family_name]} #{name[:first_name]}"
+array = []
+csv = CSV.read("fishing_logs.csv")
+csv.each do |row|
+  if row[2] == "アジ"
+    array.push(row)
+  end
 end
-
-puts names.any? { |e| e[:first_name] == "taro" }
-
-s = gets.chomp.chars
-hash = Hash.new(0)
-
-s.each do |n|
-  hash[n] += 1
-end
-
-key, value = hash.max_by { |k, v| v }
-puts "#{key} #{value}"
