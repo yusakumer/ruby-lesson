7f6d15e3e09fcdf42e1667d8450428f577f8829e
yusaku <s.yusaku1220@gmail.com>
2026-06-23 23:50:16 +0900

    ruby 練習

--- resson-ruby/resson.rb
+++ resson-ruby/resson.rb
@@ -1,9 +1,34 @@
+# CSV読み込み、書き込み練習
+
 require 'csv'
 
 array = []
-csv = CSV.read("fishing_logs.csv")
+csv = CSV.read("fishing_logs.csv", headers: true)
 csv.each do |row|
-  if row[2] == "アジ"
+  if row["fish_name"] == "アジ"
     array.push(row)
   end
+end
+
+CSV.open("aji_list.csv", "wb") do |r|
+  r << csv.headers
+  array.each do |row|
+    r << row
+  end
+end
+
+CSV.open("big_fish_summary.csv", "wb") do |r|
+  r << ["user_name", "count"]
+  h = Hash.new(0)
+  csv.each do |row|
+    if row["size_cm"].to_i >= 30
+      h[row["user_name"]] += 1
+
+    end
+  end
+
+  h.each do |k, v|
+    r << [k, v]
+  end
+
 end
